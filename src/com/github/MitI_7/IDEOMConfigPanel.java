3fc0923eb7cad73b4961a3e18b14c20ded20be41
MitI_7 <MitI_7@hotmail.com>
2014-12-16 18:52:39 +0900

    ファイルをブラウズできるようにした

--- src/com/github/MitI_7/IDEOMConfigPanel.java
+++ src/com/github/MitI_7/IDEOMConfigPanel.java
@@ -2,32 +2,39 @@ package com.github.MitI_7;
 
 
 import javax.swing.*;
-import javax.swing.JTextField;
 import javax.swing.JSlider;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.VirtualFile;
 
 
 public class IDEOMConfigPanel extends JComponent{
     private JPanel panel;
 
-    public JCheckBox useWallPaperCheckBox;
-    public JTextField imagePathTextField;
-    public JSlider imageOpacitySlider;
-    public JComboBox imagePositionComboBox;
+    private JCheckBox useWallPaperCheckBox;
+    private TextFieldWithBrowseButton imagePath;
+    private JSlider imageOpacitySlider;
+    private JComboBox imagePositionComboBox;
     private JComboBox comboBox1;
 
     public IDEOMConfigPanel(IDEOMConfig ideomConfig) {
         useWallPaperCheckBox.setSelected(ideomConfig.state.useWallPaper);
-        imagePathTextField.setText(ideomConfig.state.imagePath);
+        imagePath.setText(ideomConfig.state.imagePath);
         imageOpacitySlider.setValue((int)(ideomConfig.state.imageOpacity * 100));
         imagePositionComboBox.setSelectedIndex(ideomConfig.state.imagePositionNo);
+        imagePath.addBrowseFolderListener(
+                "Title", "Description", null,
+                new FileChooserDescriptor(true, false, false, false, false, false)
+        );
     }
 
     public IDEOMConfig.State get_state() {
         IDEOMConfig.State state = new IDEOMConfig.State();
         state.useWallPaper    = this.useWallPaperCheckBox.isSelected();
-        state.imagePath       = this.imagePathTextField.getText();
+        state.imagePath       = this.imagePath.getText();
         state.imageOpacity    = this.imageOpacitySlider.getValue() / 100.0f;
         state.imagePositionNo = this.imagePositionComboBox.getSelectedIndex();
 
